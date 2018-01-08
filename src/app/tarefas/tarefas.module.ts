f71d0dab8f6aa4c95beed0e4dbfb3f0b5fe03640
sidneibiz <sidneibiz@gmail.com>
2018-01-08 18:22:38 -0200

    create directive angular 2

--- src/app/tarefas/tarefas.module.ts
+++ src/app/tarefas/tarefas.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 
-import { TarefaService } from './shared';
+import { TarefaService, TarefaConcluidaDirective } from './shared';
 import { ListarTarefaComponent } from './listar';
 import { CadastrarTarefaComponent } from './cadastrar';
 import { EditarTarefaComponent } from './editar';
@@ -14,7 +14,7 @@ import { EditarTarefaComponent } from './editar';
     RouterModule,
     FormsModule
   ],
-  declarations: [ListarTarefaComponent, CadastrarTarefaComponent, EditarTarefaComponent],
+  declarations: [ListarTarefaComponent, CadastrarTarefaComponent, EditarTarefaComponent, TarefaConcluidaDirective],
   providers: [TarefaService]
 })
 export class TarefasModule { }
